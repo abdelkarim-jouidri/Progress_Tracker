9fed41042e1c56b181f3772ce8a8df05f4bc242f
abdelkrim-jouidri <abdelkrimjouidri@gmail.com>
2023-11-13 00:02:42 +0100

    added the spring-config xml file to the file structure

--- src/main/java/com/example/progress_tracker/Models/Goal.java
+++ src/main/java/com/example/progress_tracker/Models/Goal.java
@@ -4,12 +4,46 @@ import lombok.Data;
 
 @Data
 public class Goal {
-    private int id;
+    private Long id;
     private String description;
-    private CompletionStatus completionStatus;
+    private boolean completionStatus;
 
-    public enum CompletionStatus{
-        IN_PROGRESS,
-        COMPLETED
+    public Goal(Long id, String description) {
+        this.id = id;
+        this.description = description;
+        this.completionStatus = false; // Default completion status is false
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompletionStatus() {
+        return completionStatus;
+    }
+
+    public void setCompletionStatus(boolean completionStatus) {
+        this.completionStatus = completionStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Goal{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", completionStatus=" + completionStatus +
+                '}';
     }
 }
--- src/main/java/com/example/progress_tracker/Models/User.java
+++ src/main/java/com/example/progress_tracker/Models/User.java
@@ -2,10 +2,47 @@ package com.example.progress_tracker.Models;
 
 import lombok.Data;
 
-@Data
 public class User {
-    private int id;
+    private Long id;
     private String fullName;
     private String avatar;
 
+    public User(Long id, String fullName, String avatar) {
+        this.id = id;
+        this.fullName = fullName;
+        this.avatar = avatar;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", avatar='" + avatar + '\'' +
+                '}';
+    }
 }
